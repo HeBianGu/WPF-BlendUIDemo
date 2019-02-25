b4fae2bb02d5819dcaf8bae249b44483e0491704
HeBianGu <908293466@qq.com>
2019-02-25 18:06:18 +0800

    1111

---
+++ Source/HeBianGu.Blend.UIDemo/SampleData/SampleDataSource/SampleDataSource.xaml.cs
@@ -0,0 +1,162 @@
+﻿//   *********  请勿修改此文件   *********
+//   此文件由设计工具再生成。更改
+//   此文件可能会导致错误。
+namespace Expression.Blend.SampleData.SampleDataSource
+{
+    using System; 
+    using System.ComponentModel;
+
+// 若要在生产应用程序中显著减小示例数据涉及面，则可以设置
+// DISABLE_SAMPLE_DATA 条件编译常量并在运行时禁用示例数据。
+#if DISABLE_SAMPLE_DATA
+    internal class SampleDataSource { }
+#else
+
+    public class SampleDataSource : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public SampleDataSource()
+        {
+            try
+            {
+                Uri resourceUri = new Uri("/HeBianGu.Blend.UIDemo;component/SampleData/SampleDataSource/SampleDataSource.xaml", UriKind.RelativeOrAbsolute);
+                System.Windows.Application.LoadComponent(this, resourceUri);
+            }
+            catch
+            {
+            }
+        }
+
+        private ItemCollection _Collection = new ItemCollection();
+
+        public ItemCollection Collection
+        {
+            get
+            {
+                return this._Collection;
+            }
+        }
+
+        private string _name = string.Empty;
+
+        public string name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                if (this._name != value)
+                {
+                    this._name = value;
+                    this.OnPropertyChanged("name");
+                }
+            }
+        }
+
+        private string _value = string.Empty;
+
+        public string value
+        {
+            get
+            {
+                return this._value;
+            }
+
+            set
+            {
+                if (this._value != value)
+                {
+                    this._value = value;
+                    this.OnPropertyChanged("value");
+                }
+            }
+        }
+
+        private string _age = string.Empty;
+
+        public string age
+        {
+            get
+            {
+                return this._age;
+            }
+
+            set
+            {
+                if (this._age != value)
+                {
+                    this._age = value;
+                    this.OnPropertyChanged("age");
+                }
+            }
+        }
+    }
+
+    public class ItemCollection : System.Collections.ObjectModel.ObservableCollection<Item>
+    { 
+    }
+
+    public class Item : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string _name = string.Empty;
+
+        public string name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                if (this._name != value)
+                {
+                    this._name = value;
+                    this.OnPropertyChanged("name");
+                }
+            }
+        }
+
+        private bool _value = false;
+
+        public bool value
+        {
+            get
+            {
+                return this._value;
+            }
+
+            set
+            {
+                if (this._value != value)
+                {
+                    this._value = value;
+                    this.OnPropertyChanged("value");
+                }
+            }
+        }
+    }
+#endif
+}
